df12c3a98452ba4c7191953118334eb08e9057b3
nttbdrk25 <31524064+nttbdrk25@users.noreply.github.com>
2025-07-12 17:35:38 +0700

    Update MRAP_Attention.py

--- models/MRAP_Attention.py
+++ models/MRAP_Attention.py
@@ -1,7 +1,7 @@
 import torch
 import math
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as F 
 from .common import *
 class Flatten(nn.Module):
     def forward(self, x):
